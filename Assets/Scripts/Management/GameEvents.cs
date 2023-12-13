668bb5086a29c1925343d8b0880a26b68adb4d7f
Jack Sell <98410625+jhdunc@users.noreply.github.com>
2023-12-13 12:24:23 +0100

    Organization

---
+++ Assets/Scripts/Management/GameEvents.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onSoilTill;
+    public void SoilTill()
+    {
+        if(onSoilTill != null)
+        {
+            onSoilTill();
+        }
+    }
+}
