328a986451a5b1a031898cdba36e5d59d0e5cbee
Jack Sell <98410625+jhdunc@users.noreply.github.com>
2023-12-17 14:42:30 +0100

    Licked a few things
    Added Bench
    Added Soil
    started veggie script to peep which veggies have special things in states

---
+++ Assets/Scripts/Items/VeggieClass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeggieClass : ItemClass
+{
+    public bool scaleGrow;
+    public bool hasMulti;
+    public int stage;
+
+    private void Start()
+    {
+        if (stage == 4)
+        {
+
+        }
+    }
+}
