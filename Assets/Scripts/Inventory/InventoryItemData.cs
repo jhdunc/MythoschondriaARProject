ff82bd17ef19df0be93df1364ef0667b1eacd5ce
UnofficialTranslator <cooldaffie9@gmail.com>
2024-01-15 11:30:02 +0100

    Revert "Merge branch 'main' of https://github.com/jhdunc/MythoschondriaARProject"
    This reverts commit 0af359fa847517ee47998e04de1477f8bbc23157, reversing
    changes made to 47ecd546c5feaf2c5ffab356c60a489b31b95298.

--- Assets/Scripts/Inventory/InventoryItemData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = "Inventory System/Inventory Items")]
-public class InventoryItemData : ScriptableObject
-{
-    public ItemClass itemInfo;
-}
--- Assets/Scripts/Items/ItemClass.cs
+++ Assets/Scripts/Items/ItemClass.cs
@@ -13,3 +13,5 @@ public class ItemClass : ScriptableObject
     public Image icon;
     
 }
+
+
