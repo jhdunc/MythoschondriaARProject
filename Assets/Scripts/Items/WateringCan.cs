268fd2a754f0c91482c65e2ecff719b6a10218b8
Jaide64 <j.t.opara@st.hanze.nl>
2024-01-17 19:33:41 +0100

    (update) Watering can
    - particle system to make water look like it is coming out of can if it is tilted

---
+++ Assets/Scripts/Items/WateringCan.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringCan : MonoBehaviour
+{
+    ParticleSystem spillLiquid;
+    ParticleSystem.EmissionModule em;
+    [SerializeField] float angleOfCan;
+
+    void Start()
+    {
+        spillLiquid = GetComponent<ParticleSystem>();
+        em = spillLiquid.emission;
+    }
+
+    void Update()
+    {
+        em.enabled = Vector3.Angle(Vector3.down, transform.right) <= angleOfCan;
+    }
+}
