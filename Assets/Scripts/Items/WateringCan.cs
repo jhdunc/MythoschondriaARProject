fe146fa30235227dcad71d27100b9040120bdf49
Jaide64 <j.t.opara@st.hanze.nl>
2024-01-22 13:29:07 +0100

    (update) Added bird flying and lighting
    - added another flying bird
    - added lighting
    - added notes to watering can code (code wasn't altered)

--- Assets/Scripts/Items/WateringCan.cs
+++ Assets/Scripts/Items/WateringCan.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class WateringCan : MonoBehaviour
 {
-    ParticleSystem spillLiquid;
-    ParticleSystem.EmissionModule em;
-    [SerializeField] float angleOfCan;
+    ParticleSystem spillLiquid; // what particle system is supposed to do 
+    ParticleSystem.EmissionModule em; // allows us to control the emmissions aka the functionality of the particle system 
+    [SerializeField] float angleOfCan; // the angle of the can will dictate when the particle system is active which can be adjusted 
 
     void Start()
     {
-        spillLiquid = GetComponent<ParticleSystem>();
-        em = spillLiquid.emission;
+        spillLiquid = GetComponent<ParticleSystem>(); // if spill liquid is occuring use the particle system attached to the watering can 
+        em = spillLiquid.emission; 
     }
 
     void Update()
     {
-        em.enabled = Vector3.Angle(Vector3.down, transform.right) <= angleOfCan;
+        em.enabled = Vector3.Angle(Vector3.down, transform.right) <= angleOfCan; // the particles will only be visable when the object is transformed to a specific angle 
     }
 }
